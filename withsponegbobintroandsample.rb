f9c5a926b4acb976d8f63a53d820f4f477d15d0f
austinfulks <54602734+austinfulks@users.noreply.github.com>
2020-01-24 15:39:25 -0800

    Add files via upload

---
+++ withsponegbobintroandsample.rb
@@ -0,0 +1,44 @@
+# Welcome to Sonic Pi v3.1
+use_bpm 80
+
+sample"C:/Users/austin_fulks/Downloads/challenge_d/imagine.wav"
+sleep 10
+
+
+live_loop:b do
+  play:B5
+  play:D5
+  sleep 1
+  play:D5
+  play:B4
+  play:D4
+  sleep 0.5
+  play:Fs5
+  sleep 1
+  play:E5
+  sleep 0.5
+  play:Cs5
+  sleep 1
+  play:D5
+  play:Fs4
+  sleep 2
+  play:D5
+  play:D4
+  sleep 1
+  play:A5
+  play:D5
+  play:Fs4
+  sleep 1
+  play:Fs5
+  sleep 1
+  play:E5
+  sleep 1
+  play:E6
+  sleep 1
+  play:B4
+  play:E4
+  sleep 3
+  sample"C:/Users/austin_fulks/Downloads/challenge_d/Spongebob Laugh.wav"
+end
+
+
